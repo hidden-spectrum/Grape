962016ae2f1bebaf15943ffc5e6d38b72d7c2fbe
Zhen Li <craig.lizhen@outlook.com>
2025-01-13 20:34:37 -0800

    Add `Identifiable` conformance to `_IdentifiableNever`

--- Sources/Grape/Contents/_IdentifiableNever.swift
+++ Sources/Grape/Contents/_IdentifiableNever.swift
@@ -3,11 +3,21 @@ public enum _IdentifiableNever<ID: Hashable> { }
 extension _IdentifiableNever: GraphContent {
     public typealias NodeID = ID
     
+    @inlinable
     public var body: Self {
         fatalError()
     }
     
+    @inlinable
     public func _attachToGraphRenderingContext(_ context: inout _GraphRenderingContext<NodeID>) {
+        fatalError()
+    }
+}
+
+
+extension _IdentifiableNever: Identifiable {
+    @inlinable
+    public var id: ID {
         fatalError()
     }
 }
